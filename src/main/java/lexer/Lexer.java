10773b5f27690cc55577e707e79b7c479b306910
srnam <smarkovic8424rn@raf.rs>
2025-11-03 00:09:04 +0100

    Dodate neke funkcionalnosti

--- src/main/java/lexer/Lexer.java
+++ src/main/java/lexer/Lexer.java
@@ -20,14 +20,20 @@ public class Lexer {
             Map.entry("niz", TokenType.NIZ),
             Map.entry("pogodak", TokenType.POGODAK),
             Map.entry("moj", TokenType.MOJ),
+            Map.entry("moje", TokenType.MOJE),
             Map.entry("zapocni_igru", TokenType.ZAPOCNI_IGRU),
             Map.entry("zavrsi_igru", TokenType.ZAVRSI_IGRU),
             Map.entry("funkcija", TokenType.FUNKCIJA),
             Map.entry("ako", TokenType.AKO),
             Map.entry("ili", TokenType.ILI),
+            Map.entry( "i", TokenType.I),
             Map.entry("inace", TokenType.INACE),
             Map.entry("radi", TokenType.RADI),
-            Map.entry("vrati", TokenType.VRATI)
+            Map.entry("vrati", TokenType.VRATI),
+            Map.entry("upisi", TokenType.UPISI),
+            Map.entry("napisi", TokenType.NAPISI),
+            Map.entry("false", TokenType.BOOL_LIT),
+            Map.entry("true", TokenType.BOOL_LIT)
     );
 
     public Lexer(String source) {
@@ -58,17 +64,16 @@ public class Lexer {
             case ';' -> add(TokenType.SEMICOLON);
             case ':' -> add(TokenType.TYPE_COLON);
             case '+' -> add(TokenType.ADD);
-            case '-' -> add(sc.match('>') ? TokenType.ASSIGN : TokenType.SUBTRACT);
+            case '-' -> add(TokenType.SUBTRACT);
             case '*' -> add(TokenType.MULTIPLY);
             case '/' -> add(TokenType.DIVIDE);
             case '%' -> add(TokenType.PERCENT);
             case '<' -> add(sc.match('=') ? TokenType.LE : TokenType.LT);
             case '>' -> add(sc.match('=') ? TokenType.GE : TokenType.GT);
-            case '=' -> add(TokenType.EQ);
-            case '!' -> {
-                if (sc.match('=')) add(TokenType.NEQ);
-                else throw error("Unexpected '!'");
-            }
+            case '=' -> add(sc.match('=') ? TokenType.EQ:TokenType.ASSIGN);
+            case '!' -> add(sc.match('=') ? TokenType.NEQ:TokenType.NE);
+            case '"' -> string();
+            case '\'' -> charLiteral();
             case '\n' -> tokens.add(new Token(
                     TokenType.NEWLINE, "\n", null, sc.getStartLine(), sc.getStartCol(), sc.getStartCol()
             ));
@@ -90,6 +95,46 @@ public class Lexer {
             throw error("Error: Character in int literal");
         }
         addLiteralInt(text);
+    }
+
+    private void string() {
+        StringBuilder literal = new StringBuilder();
+
+        while (sc.peek() != '"' && !sc.isAtEnd()) {
+            literal.append(sc.peek());
+            sc.advance();
+        }
+        if (sc.isAtEnd()) throw error("Unterminated string literal");
+        sc.advance();
+        String text = literal.toString();
+        if (literal.isEmpty()) {
+            throw error("String literal must not be empty");
+        }
+        tokens.add(new Token(TokenType.STR_LIT, text, null,
+                sc.getStartLine(), sc.getStartCol(), sc.getCol() - 1));
+
+    }
+    private void charLiteral(){
+       if (sc.isAtEnd())
+            throw error("Unterminated char literal");
+
+        while (sc.peek() == ' ' && !sc.isAtEnd()) {
+            sc.advance();
+        }
+
+        if (sc.isAtEnd())
+            throw error("Unterminated char literal");
+        char value = sc.peek();
+        sc.advance();
+        while (sc.peek() == ' ' && !sc.isAtEnd()) {
+            sc.advance();
+        }
+        if (sc.peek() != '\'')
+            throw error("Char literal must contain exactly one character");
+        sc.advance();
+        tokens.add(new Token(TokenType.CH_LIT, String.valueOf(value), null,
+                sc.getStartLine(), sc.getStartCol(), sc.getCol() - 1));
+        
     }
 
     private void identifier() {
@@ -117,6 +162,9 @@ public class Lexer {
         tokens.add(new Token(TokenType.INT_LIT, literal, Integer.valueOf(literal),
                 sc.getStartLine(), sc.getStartCol(), sc.getCol() - 1));
     }
+
+
+
 
     private RuntimeException error(String msg) {
         String near = source.substring(sc.getStartIdx(), Math.min(sc.getCur(), source.length()));
